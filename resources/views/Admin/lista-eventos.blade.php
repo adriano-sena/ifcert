b804f442ddf459d9f6551a6f97ea935f6f776862
adriano-sena <adrsena96@gmail.com>
2020-03-09 22:44:39 -0300

    Alterações na listagem de eventos

--- resources/views/Admin/lista-eventos.blade.php
+++ resources/views/Admin/lista-eventos.blade.php
@@ -7,28 +7,21 @@
 @section('conteudo')
 
 <a href="{{route('admin.evento.create')}}" class="btn btn-success">Criar Loja</a>
-    <section id="tabela">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Evento</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($eventos as $evento)
-                <tr>
-                    <td>{{$evento->id}}</td>
-                    <td>{{$evento->titulo}}</td>
-                    <div class="d-flex justify-content-end">
-                        <td><a href="{{route('admin.evento.editar', ['evento' => $evento->id])}}" class="btn btn-primary">Editar</a></td>
-                        <td><a href="{{route('admin.evento.editar', ['evento' => $evento->id])}}" class="btn btn-primary">Atividades</a></td>
-                        <td><a href="{{route('admin.evento.deleta', ['evento' => $evento->id])}}" class="btn btn-danger">Deletar</a></td>
-                    </div>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <section id="lista">
+        
+    <ul class="list-group">
+        @foreach ($eventos as $evento)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span id="titulo-evento">{{ $evento->titulo }}</span>
+                
+            </li>
+        @endforeach
+    </ul>            
+    
+    
+        {{-- <a href="{{route('admin.evento.editar', ['evento' => $evento->id])}}" class="btn btn-primary">Editar</a>
+    <a href="{{route('admin.evento.editar', ['evento' => $evento->id])}}" class="btn btn-primary">Atividades</a>
+    <a href="{{route('admin.evento.deleta', ['evento' => $evento->id])}}" class="btn btn-danger">Deletar</a> --}}
+            
     </section>
 @endsection
