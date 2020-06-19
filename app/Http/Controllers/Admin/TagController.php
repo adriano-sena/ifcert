bd99eb43fbdd21ab72828c5cca35b70631722be9
adriano-sena <adrsena96@gmail.com>
2020-06-19 16:48:11 -0300

    core: validação tag concluida

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Evento;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 use phpDocumentor\Reflection\Types\Boolean;
 
 
@@ -14,26 +15,46 @@ class TagController extends Controller{
 
     /**
      * Recebe e processa a requisição ajax
+	 * Valida o a tag -> na validação foi utilizada a criação
+	 * de um validador manual, pois seria apenas um item a ser verificado
+	 * e para deixar o código mais compreensível
      * retorna o objeto criado como json
      * para ser exibido na página do certificado
      */
     public function store(Request $request, Evento $evento){
 
-         $request->tag;
-         $request->evento;
 
-		$tag['success'] = 'true';
-		$tag['tag'] = $request->tag;
-		$tag['evento'] = $request->evento;
-		echo json_encode($tag);
+		if (!$this->validaTag($request->tag)) {
+			$tag['success'] = false;
+			$tag['message'] = "A tag não corresponde ao padrão estabelecido";
+			$tag['tag'] = $request->tag;
+			echo json_encode($tag);
+			return;
+
+		}else{
+			$tag['success'] = true;
+			$tag['tag'] = $request->tag;
+			$tag['evento'] = $request->evento;
+			$tag['message'] = "A tag corresponde as especificações";
+			echo json_encode($tag);
+			return;
+		}
+		return;
     }
 
-    private function validaTag(String $tag): Boolean {
 
-    	trim($tag);//limpa os espaços em branco
-		if (!Str::startsWith($tag, "#")){
-			return false;
-		}
-    	return true;
+//	private function sanitizeTag(String $tag): String {
+//
+//		$tagLimpa = preg_replace( array( '/[ ]/' , '/[^A-Za-z0-9\-]/' ) , array( '' , '' ) , $tag );
+//
+//		return $tagLimpa;
+//	}
+
+	private function validaTag(String $tag) {
+
+    	$padrao = "/^#[a-z]{1,15}[^\s\"\-\@#$%¨&*()+§=ª[º}{¬°<>.,\|]+$/";
+    	return preg_match($padrao, $tag);
 	}
+
+
 }
--- resources/js/tags.js
+++ resources/js/tags.js
@@ -1,39 +1,40 @@
-
 /**
  * Ajax para criação das tags
  */
 
 
-$(function(){
-    $('#form').submit(function(event){
-        event.preventDefault();
+$(function () {
+	$('#form').submit(function (event) {
+		event.preventDefault();
 
-        //Busque no formulario $this o valor do input com id = tag
+		//Busque no formulario $this o valor do input com id = tag
 
-        $.ajax({
-            url: "/admin/tags/store",
-            type: "post",
-            data: $(this).serialize(),
-            dataType: 'json',
-            success: function(response){
-                //adicionar a tag criada em uma lista (ul)
-                let tag = response['tag'];
-				let listItem = $('<li></li>').text(tag);
-                $('#lista').append(listItem);
+		$.ajax({
+			url: "/admin/tags/store",
+			type: "post",
+			data: $(this).serialize(),
+			dataType: 'json',
+			success: function (response) {
 
-                console.log(response);
-            }
-        });
+				if (response.success === true ) {
+					let tag = response['tag'];
+					let listItem = $('<li></li>').text(tag);
+					$('#lista').append(listItem);
+				}else{
+					alert("Erro ! " + response.message);
+				}
 
-    });
+
+			}
+		});
+
+	});
 });
-
 
 
 // $(function(){
 //     $('#form').submit(function(event){
 //         event.preventDefault();
-
 //         //Busque no formulario $this o valor do input com id = tag
 //         var tag = $(this).find('input#tag').val();
 //         var evento = $(this).find('input#evento-id').val();
